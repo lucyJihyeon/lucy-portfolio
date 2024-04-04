4d1414e845efdcac918bb3684771ab97f126ec17
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-03 17:47:29 -0700

    navbar style edited

--- Main/src/components/Header/index.jsx
+++ Main/src/components/Header/index.jsx
@@ -2,17 +2,15 @@ import logo from "../../assets/logo/lucy-logo.png";
 
 function Header(props) {
   return (
-    <header className="flex-row space-between px-1">
+    <header className="flex-row justify-between grid grid-cols-12">
+      {/* container class for responsive  */}
       <div className="container mx-auto">
-        <div className="grid grid-cols-12">
-          <div className="col-span-3">
-            <img
-              src={logo}
-              alt="Logo"
-              style={{ width: "60px", height: "auto", margin: "10px" }}
-            />
-          </div>
-          <div className="col-span-9">{props.children}</div> 
+        <div className="col-span-12 flex items-center">
+          
+          <div className="col-span-9">
+            <img src={logo} alt="Logo" style={{ width:"70px"}}/>
+              {props.children}
+          </div> 
         </div>
       </div>
       
