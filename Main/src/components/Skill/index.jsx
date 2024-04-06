4f8fc9d133e8ef139bbe6189f99a76b4ea7f6fd8
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-06 01:40:59 -0700

    rendering front-end skills

--- Main/src/components/Skill/index.jsx
+++ Main/src/components/Skill/index.jsx
@@ -70,15 +70,11 @@ function Skill() {
     <div className="flex justify-center items-center w-full">
       {isSmallScreen ? (
         // Render vertical carousel for small screens
-
         <div className="vertical-carousel my-44 p-6 container-color rounded-box">
-          <div className="flex justify-center flex-col items-center">
-            <p className="mt-2 text-5xl">Front-end</p>
-            <p className="mt-2 mb-10 text-5xl">Skills</p>
-          </div>
           <div className="h-custom mb-10 carousel carousel-vertical rounded-box">
+            {/* rendering carousel item */}
             <div className="carousel-item h-full">
-              <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" />
+              {/* font-awesome icon */}
               <FontAwesomeIcon
                 icon={faSortDown}
                 size="2xl"
@@ -91,15 +87,31 @@ function Skill() {
                   left: "70%",
                 }}
               />
+
+              <div className="flex flex-col items-center">
+                <div className="flex justify-center flex-col items-center">
+                  <p className="mt-2 text-5xl">Front-end</p>
+                  <p className="mt-2 mb-10 text-5xl">Skills</p>
+                </div>
+                {/* rendering skills icon items */}
+                {front.map((skill, index) => (
+                  <div key={index} className=" sm:max-w-sm:w-1/3">
+                    <div className="container-color sm: w-36 carousel-item h-full flex-col">
+                      <img
+                        src={skill.path}
+                        alt={skill.name}
+                        className="w-full"
+                      />
+                      <p className="text-center m-0">{skill.name}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
 
-            <div className="carousel-item h-full">
-              <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" />
-            </div>
+            <div className="carousel-item h-full"></div>
 
-            <div className="carousel-item h-full">
-              <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" />
-            </div>
+            <div className="carousel-item h-full"></div>
           </div>
         </div>
       ) : (
